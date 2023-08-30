e97905558b0765b3379994193fa20d51bfadf784
Joaquin Vidal <joaquin.vidal@decemberlabs.com>
2023-08-30 15:50:48 -0300

    initial commit

---
+++ app/src/main/java/com/example/movies/home/components/MoviePresentation.kt
@@ -0,0 +1,11 @@
+package com.example.movies.home.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.movies.model.Movie
+
+@Composable
+fun MoviePresentation(
+    movie: Movie,
+    modifier: Modifier = Modifier){
+}
