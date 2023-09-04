657bfceba9f35277f3205c658d88e528a208d1ad
Joaquin Vidal <joaquin.vidal@decemberlabs.com>
2023-09-04 11:40:14 -0300

    showing toast when no internet

--- app/src/main/java/com/example/movies/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/movies/ui/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.movies.ui.home
 
+import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
@@ -20,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -37,12 +39,19 @@ import java.time.LocalDate
 fun HomeScreen(onMoviePressed: (Movie) -> Unit) {
     val viewModel: HomeViewModel = hiltViewModel()
     val movies by viewModel.movies.collectAsState(initial = emptyList())
+    val context = LocalContext.current
 
     LaunchedEffect(key1 = LocalDate.now().dayOfMonth, key2 = LocalDate.now().month, block = {
         withContext(Dispatchers.IO) {
             viewModel.updateMovies()
         }
     })
+
+    LaunchedEffect(Unit) {
+        viewModel.systemMessage.collect { message ->
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        }
+    }
 
     LazyVerticalGrid(
         columns = GridCells.Adaptive(minSize = 150.dp),
@@ -59,7 +68,7 @@ fun HomeScreen(onMoviePressed: (Movie) -> Unit) {
                 fontSize = 32.sp,
                 textAlign = TextAlign.Start,
                 color = Color.Black,
-                modifier = Modifier.padding(top = 8.dp),
+                modifier = Modifier.padding(top = 32.dp),
             )
         })
 
--- app/src/main/java/com/example/movies/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/movies/ui/home/HomeViewModel.kt
@@ -3,15 +3,24 @@ package com.example.movies.ui.home
 import androidx.lifecycle.ViewModel
 import com.example.movies.repository.MoviesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(private val moviesRepository: MoviesRepository): ViewModel() {
+class HomeViewModel @Inject constructor(private val moviesRepository: MoviesRepository) : ViewModel() {
 
     val movies = moviesRepository.movies
 
-    suspend fun updateMovies(){
-        moviesRepository.updateMovies()
+    private val _systemMessage = MutableSharedFlow<String>(extraBufferCapacity = 1)
+    val systemMessage = _systemMessage.asSharedFlow()
+
+    suspend fun updateMovies() {
+        try {
+            moviesRepository.updateMovies()
+        } catch (e: Exception) {
+            e.message?.let { _systemMessage.emit(it) }
+        }
     }
 
 }
