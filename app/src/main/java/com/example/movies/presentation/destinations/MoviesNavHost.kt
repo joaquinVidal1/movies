dad0d4bc708c9a9cbf3b56dba291745208a91085
Joaquin Vidal <joaquin.vidal@decemberlabs.com>
2023-09-14 15:25:39 -0300

    extracted nav host

---
+++ app/src/main/java/com/example/movies/presentation/destinations/MoviesNavHost.kt
@@ -0,0 +1,54 @@
+package com.example.movies.presentation.destinations
+
+import android.net.Uri
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.movies.presentation.home.HomeScreen
+import com.example.movies.presentation.movieDetails.MovieDetailsScreen
+import com.example.movies.presentation.movieReviews.MovieReviewsScreen
+
+@Composable
+fun MoviesNavHost(
+    navController: NavHostController, modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController, startDestination = HomeDestination.route, modifier = modifier
+    ) {
+
+        composable(route = HomeDestination.route) {
+            HomeScreen(onMoviePressed = {
+                navController.navigateToMovieDetails(it.id)
+            })
+        }
+
+        composable(route = MovieDetailsDestination.routeWithArgs, arguments = MovieDetailsDestination.arguments) { _ ->
+            MovieDetailsScreen(onBackPressed = { navController.navigateUp() }, onShowReviewsPressed = { movie ->
+                navController.navigateToReviews(
+                    movieId = movie.id, moviePoster = movie.posterPath
+                )
+            })
+        }
+
+        composable(
+            route = MovieReviewsDestination.routeWithArgs, arguments = MovieReviewsDestination.arguments
+        ) { navBackStackEntry ->
+            MovieReviewsScreen(
+                onBackPressed = { navController.navigateUp() },
+                posterPath = navBackStackEntry.arguments?.getString(MovieReviewsDestination.moviePosterPathArg)
+                    ?: throw Exception("No value passed for movie poster")
+            )
+        }
+    }
+}
+
+private fun NavHostController.navigateToMovieDetails(movieId: Int) {
+    this.navigate("${MovieDetailsDestination.route}/$movieId")
+}
+
+private fun NavHostController.navigateToReviews(movieId: Int, moviePoster: String) {
+    val encodedPosterPath = Uri.encode(moviePoster)
+    this.navigate("${MovieReviewsDestination.route}/$movieId/$encodedPosterPath")
+}
--- app/src/main/java/com/example/movies/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/movies/presentation/main/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.movies.presentation.destinations.HomeDestination
 import com.example.movies.presentation.destinations.MovieDetailsDestination
 import com.example.movies.presentation.destinations.MovieReviewsDestination
+import com.example.movies.presentation.destinations.MoviesNavHost
 import com.example.movies.presentation.home.HomeScreen
 import com.example.movies.presentation.movieDetails.MovieDetailsScreen
 import com.example.movies.presentation.movieReviews.MovieReviewsScreen
@@ -42,34 +43,7 @@ class MainActivity : ComponentActivity() {
 fun MoviesApp() {
     MoviesTheme {
         val navController = rememberNavController()
-        NavHost(
-            navController = navController, startDestination = HomeDestination.route
-        ) {
-
-            composable(route = HomeDestination.route) {
-                HomeScreen(onMoviePressed = {
-                    navController.navigateToMovieDetails(it.id)
-                })
-            }
-
-            composable(route = MovieDetailsDestination.routeWithArgs, arguments = MovieDetailsDestination.arguments) { _ ->
-                MovieDetailsScreen(onBackPressed = { navController.navigateUp() },
-                    onShowReviewsPressed = { movie ->
-                        navController.navigateToReviews(
-                            movieId = movie.id,
-                            moviePoster = movie.posterPath
-                        )
-                    })
-            }
-
-            composable(route = MovieReviewsDestination.routeWithArgs, arguments = MovieReviewsDestination.arguments) { navBackStackEntry ->
-                MovieReviewsScreen(
-                    onBackPressed = { navController.navigateUp() },
-                    posterPath = navBackStackEntry.arguments?.getString(MovieReviewsDestination.moviePosterPathArg)
-                        ?: throw Exception("No value passed for movie poster")
-                )
-            }
-        }
+        MoviesNavHost(navController = navController)
     }
 }
 
@@ -79,13 +53,4 @@ fun GreetingPreview() {
     MoviesTheme {
         MoviesApp()
     }
-}
-
-private fun NavHostController.navigateToMovieDetails(movieId: Int) {
-    this.navigate("${MovieDetailsDestination.route}/$movieId")
-}
-
-private fun NavHostController.navigateToReviews(movieId: Int, moviePoster: String) {
-    val encodedPosterPath = Uri.encode(moviePoster)
-    this.navigate("${MovieReviewsDestination.route}/$movieId/$encodedPosterPath")
 }
