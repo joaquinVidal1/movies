24afee4ccec0473c2e35a040f4caff079845dfbf
joaquin <joaquinvidal01@gmail.com>
2025-02-25 12:31:40 -0300

    removed movie from loading state

--- app/src/main/java/com/example/movies/presentation/movieDetails/MovieDetailsUiState.kt
+++ app/src/main/java/com/example/movies/presentation/movieDetails/MovieDetailsUiState.kt
@@ -8,5 +8,5 @@ sealed class MovieDetailsUiState {
 
     internal class Error(val exception: Throwable) : MovieDetailsUiState()
 
-    internal class Loading(val movie: DetailsMovie) : MovieDetailsUiState()
+    internal object Loading : MovieDetailsUiState()
 }
